43977baeb0e3f60c5dd15155e807c7c418b1e684
unknown <camerondedore@gmail.com>
2020-08-08 12:22:09 -0700

    adding disease zone tools

---
+++ Assets/C#/DiseaseZoneParticlesEditor.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class DiseaseZoneParticlesEditor : MonoBehaviour
+{
+    
+	[SerializeField]
+	float flyDensity = 10;	
+    
+
+
+    void Update()
+    {
+        if(Application.isEditor)
+		{
+			// get collider
+			var col = transform.root.GetComponent<Collider>();
+			// get particle system
+			//var particleMain = GetComponent<ParticleSystem>().main;
+			var particleShape = GetComponent<ParticleSystem>().shape;
+			var particleTriggers = GetComponent<ParticleSystem>().trigger;
+			var particleEmission = GetComponent<ParticleSystem>().emission;
+			
+			// edit particle system
+			if(col.GetType() == typeof(BoxCollider))
+			{
+				// box
+				var box = ((BoxCollider) col);
+				var volume = box.size.x * box.size.y * box.size.z;
+				particleShape.shapeType = ParticleSystemShapeType.Box;
+				particleTriggers.SetCollider(0, box);
+				//particleMain.maxParticles = Mathf.RoundToInt(flyDensity * volume);
+				particleEmission.rateOverTime = flyDensity * Mathf.Sqrt(volume);
+				particleShape.position = box.center;
+				particleShape.scale = box.size;
+			}
+			else if(col.GetType() == typeof(SphereCollider))
+			{
+				// sphere
+				var sphere = ((SphereCollider) col);
+				var volume = 1.33f * Mathf.PI * Mathf.Pow(sphere.radius, 3);
+				particleShape.shapeType = ParticleSystemShapeType.Sphere;
+				particleTriggers.SetCollider(0, sphere);
+				//particleMain.maxParticles = Mathf.RoundToInt(flyDensity * volume);
+				particleEmission.rateOverTime = flyDensity * Mathf.Sqrt(volume);
+				particleShape.position = sphere.center;
+				particleShape.scale = Vector3.one * sphere.radius;
+			}
+		}
+    }
+}
---
+++ Assets/C#/DiseaseZoneVisualizerEditor.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class DiseaseZoneVisualizerEditor : MonoBehaviour
+{
+    
+	[SerializeField]
+	Mesh boxMesh,
+		sphereMesh;
+
+    
+
+    void Update()
+    {
+        if(Application.isEditor)
+		{
+			// get collider
+			var col = transform.root.GetComponent<Collider>();
+			// get mesh
+			var meshFilter = GetComponent<MeshFilter>();
+			
+			// edit visualizer
+			if(col.GetType() == typeof(BoxCollider))
+			{
+				// box
+				var box = ((BoxCollider) col);
+				meshFilter.mesh = boxMesh;
+				transform.localPosition = box.center;
+				transform.localScale = box.size;
+			}
+			else if(col.GetType() == typeof(SphereCollider))
+			{
+				// sphere
+				var sphere = ((SphereCollider) col);
+				meshFilter.mesh = sphereMesh;
+				transform.localPosition = sphere.center;
+				transform.localScale = Vector3.one * sphere.radius * 2;
+			}
+		}
+    }
+}
