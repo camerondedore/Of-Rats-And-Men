e11fffcd675c03623bf4aef026158c85bbe464df
unknown <camerondedore@gmail.com>
2020-09-30 10:31:00 -0700

    using gameobject to prefab tool

---
+++ Assets/C#/Tools/PropBrush.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropBrush : MonoBehaviour
+{
+    
+	[SerializeField]
+	GameObject[] props;
+	[SerializeField]
+	float xzScaleMin,
+		xzScaleMax,
+		yScaleMin,
+		yScaleMax,
+		rotationMax;
+	RaycastHit rayHit;
+	GameObject lastPropPlaced;
+	int propIndex;
+
+
+
+	void Start()
+	{
+		propIndex = Random.Range(0, props.Length);
+	}
+
+
+
+	void Update()
+	{
+		// draw
+		if(Input.GetKeyDown(KeyCode.Mouse0))
+		{
+			Physics.Raycast(transform.position, transform.forward, out rayHit, 100);
+
+			if(rayHit.collider != null)
+			{
+				var prop = Instantiate(props[propIndex], rayHit.point, Quaternion.identity) as GameObject;
+				lastPropPlaced = prop;
+				
+				// apply scale
+				var xzScale = Random.Range(xzScaleMin, xzScaleMax);
+				var yScale = Random.Range(yScaleMin, yScaleMax);
+				prop.transform.localScale = new Vector3(xzScale, yScale, xzScale);
+
+				// apply rotation
+				var xRot = Random.Range(-rotationMax, rotationMax);
+				var zRot = Random.Range(-rotationMax, rotationMax);
+				var yRot = Random.Range(0f, 359f);
+				prop.transform.rotation = Quaternion.Euler(new Vector3(xRot, yRot, zRot));
+
+				// get new prop
+				var lastPropIndex = propIndex;
+				while(lastPropIndex == propIndex)
+				{
+					propIndex = Random.Range(0, props.Length);
+				}
+			}
+		}
+
+		// delete
+		if(Input.GetKeyDown(KeyCode.Mouse1))
+		{
+			Destroy(lastPropPlaced);
+		}
+	}
+}
