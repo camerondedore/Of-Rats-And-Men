fe4883eccf169b132cf2f1a2b673d8bbe2ab2e19
unknown <camerondedore@gmail.com>
2020-10-04 15:02:18 -0700

    working on first level + misc

---
+++ Assets/C#/Heirloom.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heirloom : MonoBehaviour, IPickup
+{
+    
+	[SerializeField]
+	string heirloomName;
+
+
+
+	void Start()
+	{
+		// check collection list
+		if(Heirlooms.collectedHeirlooms.Contains(heirloomName))
+		{
+			Destroy(gameObject);
+		}
+	}
+
+
+
+	void FixedUpdate()
+    {
+        transform.Rotate(Vector3.up, 90 * Time.fixedDeltaTime, Space.World);
+    }
+
+
+
+	public void Pickup(Transform player)
+	{
+		// save object to collection list
+		Heirlooms.CollectHeirloom(heirloomName);
+		Destroy(gameObject);
+	}
+}
+
---
+++ Assets/C#/Heirlooms.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary; 
+using System.IO;
+
+public class Heirlooms : MonoBehaviour
+{
+    
+	public static List<string> collectedHeirlooms = new List<string>();
+
+
+
+	void Awake()
+	{
+		LoadCollectedHeirlooms();
+		// Debug.Log("collected heirlooms: ");
+		// foreach(var s in collectedHeirlooms)
+		// {
+		// 	Debug.Log(s);
+		// }
+		// Debug.Log(Application.persistentDataPath);
+	}
+
+
+
+	public static void CollectHeirloom(string hierloomName)
+	{
+		collectedHeirlooms.Add(hierloomName);
+		SaveCollectedHeirlooms();
+	}
+
+
+
+	public static void SaveCollectedHeirlooms() 
+	{
+		BinaryFormatter bf = new BinaryFormatter();
+		FileStream file = File.Create (Application.persistentDataPath + "/heirlooms.dwg");
+		bf.Serialize(file, collectedHeirlooms);
+		file.Close();
+	}
+
+
+
+	public static void LoadCollectedHeirlooms() 
+	{
+		if(File.Exists(Application.persistentDataPath + "/heirlooms.dwg")) 
+		{
+			BinaryFormatter bf = new BinaryFormatter();
+			FileStream file = File.Open(Application.persistentDataPath + "/heirlooms.dwg", FileMode.Open);
+			collectedHeirlooms = (List<string>)bf.Deserialize(file);
+			file.Close();
+		}
+	}
+}
