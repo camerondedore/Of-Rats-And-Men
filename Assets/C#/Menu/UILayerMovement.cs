6f2037ef0a69ae1a0065de596381cb85a59a8eb4
unknown <camerondedore@gmail.com>
2020-11-29 19:47:39 -0800

    adding menu background

---
+++ Assets/C#/Menu/UILayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class UILayerMovement : MonoBehaviour
+{
+    
+	[SerializeField]
+	float maxDisplacement = 10,
+		inputSpeed = 10;
+	[SerializeField]
+	UILayer[] layers;
+	float x,
+		y;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    
+
+    void Update()
+    {
+        x += Input.GetAxisRaw("Mouse X") * Time.deltaTime * inputSpeed;
+        y += Input.GetAxisRaw("Mouse Y") * Time.deltaTime * inputSpeed;
+
+		x = Mathf.Clamp(x, -maxDisplacement, maxDisplacement);
+		y = Mathf.Clamp(y, -maxDisplacement, maxDisplacement);
+
+		foreach(var layer in layers)
+		{
+			var newPosition = new Vector3(x, y, layer.transform.position.z) * layer.displacementMultiplier;
+			layer.transform.anchoredPosition = newPosition;
+		}
+    }
+
+
+
+	[Serializable]
+	class UILayer
+	{
+		public float displacementMultiplier = 1;
+		public RectTransform transform;
+	}
+}
