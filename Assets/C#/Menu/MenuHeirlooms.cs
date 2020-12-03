83be56ddb85f7fde10f97354330bbc5005c5a7a4
unknown <camerondedore@gmail.com>
2020-12-02 22:18:21 -0800

    adding menu heirlooms

---
+++ Assets/C#/Menu/MenuHeirlooms.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MenuHeirlooms : MonoBehaviour
+{
+    
+	[SerializeField]
+	UIHeirloom[] UIHeirlooms;
+
+
+
+	void Start()
+	{
+		foreach(var h in UIHeirlooms)
+		{
+			if(!Heirlooms.collectedHeirlooms.Contains(h.name))
+			{
+				h.display.SetActive(false);
+			}
+		}
+	}
+
+
+
+	[Serializable]
+	class UIHeirloom
+	{
+		public string name;
+		public GameObject display;
+	}
+}
