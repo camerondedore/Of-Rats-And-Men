917fa4c2f83703d4d734d4130eb0944b852c9c4d
unknown <camerondedore@gmail.com>
2021-01-27 20:27:53 -0800

    adding menu settings

---
+++ Assets/C#/Menu/MenuSettings.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Rendering.PostProcessing;
+
+public class MenuSettings : MonoBehaviour
+{
+
+	[SerializeField] Dropdown qualityDropDown;
+	[SerializeField] Toggle ssaoToggle;
+	[SerializeField] Toggle bloomToggle;
+	[SerializeField] Slider sensitivitySlider;
+	[SerializeField] PostProcessProfile ppp;
+	AmbientOcclusion ssao;
+	Bloom bloom;
+
+
+
+    void Start()
+    {
+		ppp.TryGetSettings(out ssao);
+		ppp.TryGetSettings(out bloom);
+
+        // apply settings to game world
+		qualityDropDown.SetValueWithoutNotify(Settings.currentSettings.quality);
+		ssaoToggle.SetIsOnWithoutNotify(Settings.currentSettings.ssao);
+		bloomToggle.SetIsOnWithoutNotify(Settings.currentSettings.bloom);
+		sensitivitySlider.SetValueWithoutNotify(Settings.currentSettings.sensitivity);
+
+		QualitySettings.SetQualityLevel(Settings.currentSettings.quality);
+		ssao.active = Settings.currentSettings.ssao;
+		bloom.active = Settings.currentSettings.bloom;
+		Look.lookSensitivity = Settings.currentSettings.sensitivity;
+    }
+
+
+
+	public void ChangeSettings()
+	{
+		// gather new settings from UI
+		Settings.currentSettings.quality = qualityDropDown.value;
+		Settings.currentSettings.ssao = ssaoToggle.isOn;
+		Settings.currentSettings.bloom = bloomToggle.isOn;
+		Settings.currentSettings.sensitivity = sensitivitySlider.value;
+
+		QualitySettings.SetQualityLevel(Settings.currentSettings.quality);
+		ssao.active = Settings.currentSettings.ssao;
+		bloom.active = Settings.currentSettings.bloom;
+		Look.lookSensitivity = Settings.currentSettings.sensitivity;
+
+		Settings.SaveSettings();
+	} 
+}
---
+++ Assets/C#/Settings/Settings.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary; 
+using System.IO;
+
+public class Settings : MonoBehaviour
+{
+    
+	public static playerSettings currentSettings;
+
+
+
+	void Awake()
+	{
+		LoadSettings();
+	}
+
+
+
+	public static void SaveSettings() 
+	{
+		BinaryFormatter bf = new BinaryFormatter();
+		FileStream file = File.Create (Application.persistentDataPath + "/settings.dwg");
+		bf.Serialize(file, currentSettings);
+		file.Close();
+	}
+
+
+
+	public static void LoadSettings() 
+	{
+		if(File.Exists(Application.persistentDataPath + "/settings.dwg")) 
+		{
+			BinaryFormatter bf = new BinaryFormatter();
+			FileStream file = File.Open(Application.persistentDataPath + "/settings.dwg", FileMode.Open);
+			currentSettings = (playerSettings)bf.Deserialize(file);
+			file.Close();
+		}
+		else
+		{
+			// no settings exist
+			currentSettings = new playerSettings();
+			SaveSettings();
+		}
+
+		// Debug.Log(currentSettings.quality);
+		// Debug.Log(currentSettings.ssao);
+		// Debug.Log(currentSettings.bloom);
+		// Debug.Log(currentSettings.sensitivity);
+	}
+
+
+
+	[System.Serializable]
+	public class playerSettings
+	{
+		public int quality = 5;
+		public bool ssao = true,
+			bloom = true;
+		public float sensitivity = 10;
+	}
+}
