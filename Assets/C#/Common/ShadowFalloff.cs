ce03cae52f3e16dda6f41d11c060d3b8ad221487
unknown <camerondedore@gmail.com>
2021-02-20 14:58:44 -0800

    adding building triple and starting third level

---
+++ Assets/C#/Common/ShadowFalloff.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowFalloff : MonoBehaviour
+{
+    
+	[SerializeField]
+	AnimationCurve falloffCurve;
+	Transform mainCamera;
+	Light lamp;
+
+
+
+    void Start()
+    {
+		mainCamera = Camera.main.transform;
+		lamp = GetComponent<Light>();
+    }
+
+
+
+    void Update()
+    {
+		var distaneToCamera = Vector3.Distance(transform.position, mainCamera.position);
+		var falloff = falloffCurve.Evaluate(distaneToCamera);
+        lamp.shadowStrength = falloff;
+
+		if(falloff <= 0 && lamp.shadows != LightShadows.None)
+		{
+			lamp.shadows = LightShadows.None;
+		}
+
+		if(falloff > 0 && lamp.shadows == LightShadows.None)
+		{
+			lamp.shadows = LightShadows.Hard;
+		}
+    }
+}
