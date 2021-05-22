00ab71e4f8c7aa21fb628898705b287bea2c08ac
unknown <camerondedore@gmail.com>
2021-05-21 20:48:50 -0700

    adding end arrow

---
+++ Assets/C#/EndArrow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndArrow : MonoBehaviour
+{
+
+	[SerializeField]
+	Vector3 direction;
+	[SerializeField]
+	float speed = 1;
+    Vector3 startPosition,
+		worldDirction;
+
+
+
+    void Start()
+    {
+        startPosition = transform.position;
+		worldDirction = transform.TransformDirection(direction);
+    }
+
+    
+
+    void FixedUpdate()
+    {
+        transform.position = startPosition + worldDirction * Mathf.Sin(Time.time * 2 * Mathf.PI);
+    }
+}
