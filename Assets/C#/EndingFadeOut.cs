bc48169536db10a3b3bf78809635585740396113
unknown <camerondedore@gmail.com>
2021-05-22 19:05:48 -0700

    adding ending scene

---
+++ Assets/C#/EndingFadeOut.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndingFadeOut : MonoBehaviour
+{
+    
+	[SerializeField]
+	float fadeDelay = 9,
+		animTime = 1.75f;
+	Animator anim;
+	float startTime = Mathf.Infinity;
+	bool triggered = false;
+
+
+
+    void Start()
+    {
+        startTime = Time.time;
+		anim = GetComponent<Animator>();
+    }
+
+
+
+    void Update()
+    {
+        if(!triggered && startTime + fadeDelay < Time.time)
+		{
+			triggered = true;
+			anim.SetTrigger("exit");
+		}
+
+		if(triggered && startTime + fadeDelay + animTime < Time.time)
+		{
+			SceneLoader.LoadLevel("Menu");
+		}
+    }
+}
---
+++ Assets/C#/EndingShip.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndingShip : MonoBehaviour
+{
+   
+	[SerializeField]
+	Vector3 velocity;
+	[SerializeField]
+	float noiseSpeed,
+		noiseSize;
+	Vector3 startAngles;
+
+
+    void Start()
+    {
+		startAngles = transform.eulerAngles;
+    }
+
+   
+
+    void FixedUpdate()
+    {
+        transform.position += velocity * Time.fixedDeltaTime;
+
+		var noiseChangeX = Mathf.PerlinNoise(Time.time * noiseSpeed, 1) - 0.5f;
+		var noiseChangeY = Mathf.PerlinNoise(Time.time * noiseSpeed, 10) - 0.5f;
+		var noiseChangeZ = Mathf.PerlinNoise(Time.time * noiseSpeed, 20) - 0.5f;
+		var noiseChange = new Vector3(noiseChangeX, noiseChangeY, noiseChangeZ) * noiseSize;
+
+		transform.rotation = Quaternion.Euler(startAngles + noiseChange);
+    }
+}
