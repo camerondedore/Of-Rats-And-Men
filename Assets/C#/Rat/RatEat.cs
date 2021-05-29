ba41b7a57bb1262011321c8e9cad1f73a743c54f
unknown <camerondedore@gmail.com>
2021-05-29 14:59:46 -0700

    working on char rat, adding rat eat

---
+++ Assets/C#/Rat/RatEat.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RatEat : MonoBehaviour
+{
+    
+	Animator anim;
+
+
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+		// randomize animation
+		anim.SetFloat("idleOffset", Random.Range(0f, 1f));
+    }
+}
